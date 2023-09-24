54e4dba98e8b1ce920cde11a3cc87c2a2cd4607f
muskan1311 <48205543+muskan1311@users.noreply.github.com>
2023-09-24 23:02:40 +0530

    Update christmas_tree.py

--- christmas_tree.py
+++ christmas_tree.py
@@ -1,1 +1,18 @@
+#This will reveal an image!
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
 
+
+for image in picture:
+  for pixel in image:
+    if (pixel):
+      print('*', end ="")
+    else:
+      print(' ', end ="")
+  print('')
