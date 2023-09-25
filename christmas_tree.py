f64a3e4bc5f054201f47eb5bf17346fb5a82c03a
muskan1311 <48205543+muskan1311@users.noreply.github.com>
2023-09-25 14:51:28 +0530

    Update christmas_tree.py

--- christmas_tree.py
+++ christmas_tree.py
@@ -8,11 +8,14 @@ picture = [
   [0,0,0,1,0,0,0]
 ]
 
+def christmas_tree():
+  for image in picture:
+    for pixel in image:
+      if (pixel):
+        print('*', end ="")
+      else:
+        print(' ', end ="")
+     print('')
 
-for image in picture:
-  for pixel in image:
-    if (pixel):
-      print('*', end ="")
-    else:
-      print(' ', end ="")
-  print('')
+# To print as many as you like
+christmas_tree()
